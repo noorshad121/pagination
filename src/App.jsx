5978ae09e2c6b7dda383c876791d24cba1bf4b76
Noorshad <studentnoorshad@gmail.com>
2026-06-04 13:37:44 +0530

    pagination

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, {useState, useEffect } from 'react';
+import './App.css';
+const App = () => {
+    
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const fetchProducts = async () => {
+    try {
+      const res = await fetch('https://dummyjson.com/products');
+      const data = await res.json();
+       
+      if(data && data.products){
+        console.log(data.products)
+      setProducts(data.products);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+
+  const selectedPage = (page)=>{
+    setPage(page);
+  }
+  return (
+    <div>
+      <h1>Welcome to PageN</h1>
+         
+         <div>
+          {products.length > 0 && (
+            <div className='product'> {products.slice(page * 10 -10, page * 10).map((prod, index) => (
+               <div key={index}>
+                 <div className="card" >
+                <img src={prod.images[0]} alt="image" width="150px" />
+                <h3>{prod.title}</h3>
+                <p> &#8377; {prod.price}</p>
+                </div>
+               </div> ))} 
+               </div>
+          )}
+         </div>
+          {
+            products.length > 0 && (
+              <div className="pagination">
+                <span onClick={()=>selectedPage(page > 1 ? page - 1 : 1)}>◀️</span>
+                {[...Array(products.length / 10)].map((_, i)=>( <span className={`number ${page === i+1? 'pagination-selected':""}`} onClick={()=>selectedPage(i+1)}>{i+1}</span>))}
+                <span onClick ={()=> selectedPage(page < products.length / 10 ? page + 1 :products.length / 10)}>▶️</span>
+              </div>
+            )
+          } 
+    </div>
+  );
+};
+
+export default App;
